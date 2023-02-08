8ef5e772c0104776f5a6648b8a4bd97f5a462224
Mauro Fassi <maurofassi17@gmail.com>
2023-02-08 14:38:26 -0300

    creando dinamicamente una tabla

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
-
+const date=new Date()
 const clickBtn=document.getElementById('click')
 const stopBtn=document.getElementById('stop')
+const crearFilaBtn=document.getElementById('btnCrearFila')
 const info=document.getElementById("info")
 
+function crearFila(){
+const cuerpoTabla=document.getElementById('cuerpoTabla')
+const creaFila=document.createElement("tr")
+let creaTdNombre=document.createElement("td")
+    creaTdNombre.innerHTML=nombre
+let creaTdClicks=document.createElement("td")
+    creaTdClicks.innerHTML=contador
+let creaTdFecha=document.createElement("td")
+    creaTdFecha.innerHTML=`${date.getDate()} / ${date.getMonth()+1} / ${date.getFullYear()}`
+
+let fila =cuerpoTabla.appendChild(creaFila)
+
+    fila.appendChild(creaTdNombre)
+    fila.appendChild(creaTdClicks)
+    fila.appendChild(creaTdFecha)
+
+}
 
 
 
+
+let nombre=prompt('Ingresa tu nombre')
 let contador=0
-let guarda_contador=0
 
 let seg=setInterval(()=>{
-    seg+=1
+    contador+=1
 },1000)
 
 clickBtn.addEventListener('click',()=>{
@@ -25,13 +44,18 @@ clickBtn.addEventListener('click',()=>{
 
 
 stopBtn.addEventListener('click',()=>{
-    alert(`Hiciste ${contador} clicks en ${seg} segundos`)
+
+    alert(`Hola ${nombre} hiciste ${contador} clicks en ${seg} segundos`)
    contador=0
    numero.innerHTML=0
+
+  
    
 })
 
-
+crearFilaBtn.addEventListener('click',()=>{
+    crearFila()
+})
 
 
 
